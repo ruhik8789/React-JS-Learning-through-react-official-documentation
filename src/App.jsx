d653915c6e1f19d75e8795027b39f3e21a73440b
Ruhi K <ruhik8789@gmail.com>
2026-01-04 20:03:59 +0530

    pushed the changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-function Profile() {
-  return (
-    <img
-      src="https://media.istockphoto.com/id/1469185543/photo/science-tablet-and-portrait-of-a-male-scientist-doing-research-with-technology-in-a-medical.webp?a=1&b=1&s=612x612&w=0&k=20&c=mRObZDL23XlkyH5Kq_VteOucoGohtHxQYPtTTpBO6dc="
-      alt="Katherine Johnson"
-    />
-  );
+// import Gallery, { Profile } from "./Gallery.jsx"
+
+// export default function App() {
+//   return (
+//     <>
+//       <Gallery />
+//       <Profile />
+//     </>
+//   );
+// }
+
+const today = new Date();
+
+function formatDate(date) {
+  return new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
 }
-export default function Gallery() {
-  return (
-    <section>
-      <h1>Scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
-  )
+
+export default function TodoList() {
+  return <h1>To do list for {formatDate(today)}</h1>;
 }
---
+++ src/Gallery.jsx
@@ -0,0 +1,21 @@
+export function Profile() {
+  return (
+    <img
+      src="https://media.istockphoto.com/id/1469185543/photo/science-tablet-and-portrait-of-a-male-scientist-doing-research-with-technology-in-a-medical.webp?a=1&b=1&s=612x612&w=0&k=20&c=mRObZDL23XlkyH5Kq_VteOucoGohtHxQYPtTTpBO6dc="
+      alt="Katherine Johnson"
+    />
+  );
+}
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Scientists</h1>
+      {/* <Profile />
+      <Profile />
+      <Profile />
+      <Profile />
+      <Profile />
+      <Profile /> */}
+    </section>
+  );
+}
